073242bc2626f63c24f839f9d9ed2c37ff0cea29
PW-Phuoc <phuoc.hn106@gmail.com>
2025-07-03 07:34:33 +0700

    feat:lesson 6

---
+++ tests/lesson-6/test1.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { RegisterPage } from '../../pages/01-pom';
+
+test('Register successfully and check table data', async ({ page }) => {
+    const registerPage = new RegisterPage(page)
+    registerPage.xpathUsername = "//input[@name='username']";
+    registerPage.xpathEmail = "//input[@name='email']";
+    registerPage.xpathGenderMale = "//input[@value='male']";
+    registerPage.xpathGenderFemale = "//input[@value='female']";
+    registerPage.xpathRegisterButton = "//button[contains(text(),'Register')]";
+    registerPage.xpathRegisterPage = "//a[contains(text(), 'Register Page')]";
+
+    //xpathProductPage = "//a[contains(text(), 'Product Page')]";
+    // this.cssTodoPage = "a[href='/todo']";
+    // this.personalNote = page.locator('#personalNote');
+
+
+    await test.step('Open material page and go to register page', async () => {
+        await registerPage.openMaterialPage();
+        await registerPage.gotoPage(registerPage.xpathRegisterPage);
+    });
+
+    await test.step('Fill form and submit', async () => {
+        await registerPage.fillUsername('phuoc2');
+        await registerPage.fillEmail('phuoc2@yopmail.com');
+        await registerPage.checkGender('female');
+        await registerPage.clickRegister();
+    });
+
+    await test.step('Verify registered info in table', async () => {
+        const isDisplayed = await registerPage.isUserInfoDisplayed('phuoc2', 'phuoc2@yopmail.com');
+        expect(isDisplayed).toBeTruthy();
+    });
+});
