fffa95dcca7865453f196f48eecd2bf36c6b428c
PW-Phuoc <phuoc.hn106@gmail.com>
2025-07-17 07:07:19 +0700

    fix: drag n drop testcase

--- tests/lesson-10/test2.spec.ts
+++ tests/lesson-10/test2.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect, Locator } from '@playwright/test';
 
 test('Puzzle drag and drop game', async ({ page }) => {
-    test.setTimeout(60000);
     const pisLocator = '//div[@class="puzzle-container"]/div';
     const pisTargetLocator = '//div[@class="dropzones-container"]/div';
     await page.goto('https://material.playwrightvn.com/');
     await page.getByText('Bài học 5: Puzzle drag and drop game').click();
 
+
     for (let i = 0; i < 4; i++) {
-        await page.locator(pisLocator).nth(i).dragTo(page.locator(pisTargetLocator).nth(i));
+        await page.locator(pisLocator).nth(0).dragTo(page.locator(pisTargetLocator).nth(i));
     }
 
     page.once('dialog', async dialog => {
